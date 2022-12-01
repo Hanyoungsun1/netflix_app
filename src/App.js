33ef639bec1200f85dc83730cb216edcc754ae35
Hanyoungsun1 <morrt85@gmail.com>
2022-12-02 02:34:34 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -23,11 +23,11 @@ const App = () => {
   return (
     <div className="app">
       <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route index element={<MainPage/>}/> 
-          <Route path=":movieId" element={<DetailPage />}/>
-          <Route path="search" element={<SearchPage />}/>
-        </Route>
+           <Route path="/" element={<Layout/>}>
+           <Route index element={<MainPage/>}/> 
+           <Route path=":movieId" element={<DetailPage />}/>
+           <Route path="search" element={<SearchPage />}/>
+           </Route>
       </Routes>
      {/* { <Nav /> }
       <Banner />
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -13,34 +13,37 @@ function Banner() {
     },[]);
 
     const fetchData = async () => {
-        //현재상영중인 영화정보를 가져오기(20개 영화)
+        // 현재 상영중인 영화 정보를 가져오기(20개 영화)
       const request = await axios.get(requests.fetchNowPlaying);
       //console.log(request);
 
-      //20개 영화 중 하나의 ID를 가져오기
+      // 20개 영화 중 영화 하나의 ID를 가져오기
       const movieId = request.data.results[
-        Math.floor(Math.random() * request.data.results.length + 0)
+        
+        Math.floor(Math.random() * request.data.results.length + 0)//0~19
+      
       ].id;
 
-      //특정 영화의 더 상세한 정보를 가져오기(videos 비디오 정보도 포함)
-      const {data:movieDetail} =await axios.get(`movie/${movieId}`,{
+      // 특정 영화의 더 상세한 정보를 가져오기(videos 비디오 정보도 포함)
+      const {data:movieDetail} = await axios.get(`movie/${movieId}`,{
         params:{append_to_response: "videos"}
         });
-        console.log(movieDetail);
+        //console.log(movieDetail);
         setMovie(movieDetail);
     }
 
     const truncate = (str, n) => {
-        return str?.length > n ? str.substr(0, n -1) + "..." : str;
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
     }
   if(!isClicked){
     return (
-    <header className="banner" 
-    style={{
+    <header className="banner"
+        style={{
         backgroundImage:`url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
         backgroundPosition: "top center",
         backgroundSize:"cover",
-    }}>
+        }}>
+
         <div className="banner__contents">
             <h1 className="banner__title">
                 {movie.title || movie.name || movie.original_name}
@@ -53,51 +56,51 @@ function Banner() {
             <p className="banner__description">
                 {truncate(movie.overview, 100)}
             </p>
-            
         </div>
 
         <div className="banner--fadeBottom"></div>
     </header>
   )
   }else{
-      return(
+    return (
         <Container>
             <HomeContainer>
                 <Iframe src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`} 
                 width="640" height="360" frameborder="0" allow="autoplay; fullscreen"
-                title="YouTube video player" allowfullscreen></Iframe>
+                title="YouTube video player" allowfullscreen
+                ></Iframe>
             </HomeContainer>
         </Container>
-      )
-   }
+    )
+  }
 }
 
 const Container = styled.div`
-   display: flex;
-   flex-direction: column;
-   justify-content: center;
-   align-items: center;
-   width: 100%;
-   height: 100vh; 
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
 `;
 const HomeContainer = styled.div`
-   width: 100%;
-   height: 100%;
+  width: 100%;
+  height: 100%;
 `;
 const Iframe = styled.iframe`
-   width: 100%;
-   height: 100%;
-   z-index: -1;
-   opacity: 0.65;
-   border: none;
-   &::after{
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  opacity: 0.65;
+  border: none;
+  &::after{
     content: "";
     position: absolute;
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
-   }
+  }
 `;
 
 export default Banner;
